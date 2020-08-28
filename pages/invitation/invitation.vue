bf5830a1f120e4277e348779deff5145b1a585f5
7485102 <992643642@qq.com>
2020-08-28 16:26:41 +0800

    2020/08/28-16:26:40

---
+++ pages/invitation/invitation.vue
@@ -0,0 +1,107 @@
+<template>
+	<view class="app bg-one" :style="{'height': windowHeight + 'px'}">
+        <view class="invitation">
+            <div class="header">
+                <div class="text">
+                    邀请收益
+                </div>
+                <div class="retu"></div>
+            </div>
+            <div class="container">
+                <div class="customer">
+                    <div class="content">
+                        <div class="customer-bg">
+                            <image src="../../src/static/img/80d389_572x329.png" mode=""></image>
+                        </div>
+                        <div class="col">
+                            <div class="label">
+                                <div class="name">
+                                        我是经济商
+                                </div>
+                                <div class="time">
+                                    永久权益
+                                </div>
+                            </div>
+                            <div class="text">
+                                <div class="tit">
+                                    我的广告分红收益
+                                </div>
+                                <div class="number">
+                                    ￥100.0
+                                </div>
+                            </div>
+                            <div class="yesterday">
+                                <div class="tit">
+                                    当日分红收益
+                                </div>
+                                <div class="num">
+                                    ￥100.0
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="settlement">
+                    <div class="title">
+                        每日结算
+                    </div>
+                    <div class="list">
+                        <div class="item">
+                            <div class="text fl">
+                                <div class="label">
+                                    我的经济商团队
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="text fl">
+                                <div class="label">
+                                    直属经纪商
+                                </div>
+                                <div class="txt">
+                                    获得新手卷轴奖励一个=值13音豆
+                                </div>
+                            </div>
+                            <div class="number fr">
+                                 人数：10
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="text fl">
+                                <div class="label">
+                                    团队经纪商
+                                </div>
+                                <div class="txt">
+                                    获得团队经济商每日总广告分红的 <text>5%</text>
+                                </div>
+                            </div>
+                            <div class="number fr">
+                                 人数：10
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="button">
+                    <button type="primary">转让经纪商</button>
+                </div>
+            </div>
+        </view>
+    </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
