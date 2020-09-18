707183efb9a495a16ab8d72f9e3e66c5466faebf
sunjunjie <sunjunjie@sunjunjiedeMac.local>
2020-09-18 21:14:26 +0600

    2020/09/18-21:14:26

--- tool/store/modules/Video.js
+++ tool/store/modules/Video.js
@@ -24,20 +24,7 @@ const getters = {
 }
 
 const mutations = {
-	SET_ADD_COMMENTS(state, payload){
-		state.video_index[payload].number_of_comments = state.video_index[payload].number_of_comments + 1
-	},
-	SET_MINE_FOLLOW(state, payload){
-		console.log(state.follow_add, payload);
-		if (payload.mine_follow === 0) {
-		    state.video_index[payload.index].mine_follow = 1
-		} else if (payload.mine_follow === 1) {
-		    state.video_index[payload.index].mine_follow = 0
-		}
-	},
-	SET_VIDEO_STATUS(state, payload){
-		state.video_index[payload.index].play_status = payload.is
-	},
+	
 }
 
 const actions = {
